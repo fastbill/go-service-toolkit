03864a4892067d8db4377bfa50137103321f13c4
junedev <12543047+junedev@users.noreply.github.com>
2022-04-19 17:35:53 +0200

    allow exceptions in MustApplyDatabaseSeed check

--- database/seed.go
+++ database/seed.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 
@@ -9,19 +10,30 @@ import (
 
 // MustApplyDatabaseSeed applies all SQL queries from the given file to the currently active database.
 // If any database table besides "schema_migrations" already contains data, the seed file will not be applied.
-func MustApplyDatabaseSeed(file string, db *gorm.DB) {
+// The third argument is optional and can be used to exclude tables from checking whether data is already seeded or not.
+func MustApplyDatabaseSeed(file string, db *gorm.DB, excludedTables ...[]string) {
+	if len(excludedTables) > 1 {
+		panic(errors.New("not more than 3 arguments allowed"))
+	}
+
+	excludedTablesInSQL := []string{"schema_migrations"}
+	if len(excludedTables) == 1 {
+		excludedTablesInSQL = append(excludedTablesInSQL, excludedTables[0]...)
+	}
+
 	applySeedCheckSQL := `
 		SELECT
 			SUM(TABLE_ROWS) AS rows
 		FROM
 			information_schema.TABLES
 		WHERE
-			TABLE_SCHEMA = ? AND TABLE_NAME NOT IN ('schema_migrations')
+			TABLE_SCHEMA = ? AND TABLE_NAME NOT IN ?
 	`
 	result := struct {
 		Rows uint64
 	}{}
-	if err := db.Raw(applySeedCheckSQL, db.Migrator().CurrentDatabase()).Scan(&result).Error; err != nil {
+	statement := db.Raw(applySeedCheckSQL, db.Migrator().CurrentDatabase(), excludedTablesInSQL)
+	if err := statement.Scan(&result).Error; err != nil {
 		panic(fmt.Errorf("failed to check whether seed should be applied: %w", err))
 	}
 
